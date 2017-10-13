40f52c406d2e9ab4da99bb980374a41209ea33b7
Bill Dueber <bill@dueber.com>
2017-10-13 14:28:25 -0400

    Up the game with t.rb

--- t.rb
+++ t.rb
@@ -2,7 +2,18 @@ $:.unshift 'lib'
 require 'ht/item'
 htid = "ien.35556021287453"
 
-mets = HT::Item::MetsFile.new(File.open('35556021287453.mets.xml'))
-puts "Got mets file"
-metadata = HT::Item::Metadata.new(htid, mets: mets)
-puts "Got metadata with #{metadata.count} pagelikes"
+ids = File.open('test_ids.txt').each_line.map(&:chomp)
+ids.each do |htid|
+  begin
+    id = HT::Item::ID.new(htid)
+    size = (File.size(id.metsfile_path) / 1024.0).floor
+    print "%10dk %25s" % [size, id.id]
+    item = HT::Item.new(htid)
+  rescue => e
+    "Trouble getting #{htid}: #{e}"
+    next
+  end
+  texts = item.text_blocks.map{|str| str.force_encoding(Encoding::UTF_8)}
+  puts '  text: %10dk' % [(texts.join(' ').size / 1024.0).floor]
+end
+
